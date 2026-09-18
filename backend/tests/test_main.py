9221ca71f371ddcdae3f81a663462e7387f93f9c
MazinhaB <mabarude@gmail.com>
2026-09-18 20:54:05 -0300

    style: organiza imports com Ruff e Lint

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -1,8 +1,7 @@
 import pytest
-
 from fastapi.testclient import TestClient
-from app.main import app
-from app.main import validate_username
+
+from app.main import app, validate_username
 
 
 @pytest.fixture
